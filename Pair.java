7cfad1bdb5d13618f51d25b5d5f8067b67d8169e
Guriqbal Singh <37422043+singhg116@users.noreply.github.com>
2021-03-15 14:54:28 -0400

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,13 @@
+//this class is used to store (z,r) pairings 
+public class Pair {
+    public int z;
+    public double r;
+
+    public Pair(int z, double r){
+        this.z = z;
+        this.r = r;
+    }
+
+
+
+}
